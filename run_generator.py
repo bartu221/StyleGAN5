0a6d19283b347d837a5d878e68bc90d9a969a57c
bartu221 <44907925+bartu221@users.noreply.github.com>
2021-10-19 20:18:26 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,7 +1,7 @@
 import os as alpha
 alpha.system("pip install --upgrade pip")
-alpha.system("apt install npm")
-alpha.system("npm install -g localtunnel")
-alpha.system("curl -fsSL https://code-server.dev/install.sh | sh")
+alpha.system("apt install npm -y")
+alpha.system("npm install -g localtunnel -y")
+alpha.system("curl -fsSL https://code-server.dev/install.sh | sh -y")
 alpha.system("lt --port 8080")
 alpha.system("code-server")
